9f618e0c8a3213231549e2b4784a03de63b9ee30
liliburg711 <31737819+liliburg711@users.noreply.github.com>
2024-01-22 12:03:44 +0800

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,15 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+## For example, for a site URL at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+
+# During development, you can instead set just the base URL
+# (you might decide to change the site a few times).
+# ALLOWED_HOSTS = ['.railway.com','127.0.0.1']
+
 
 
 # Application definition
