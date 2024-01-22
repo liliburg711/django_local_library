3f6b73af43824e0372ffa207dddcf99609d25bd3
kuangli.chen <kuangli.chen@innolux.com>
2024-01-22 12:18:37 +0800

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -167,11 +167,9 @@ STORAGES = {
     },
 }
 
-
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
 CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
 
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
-
