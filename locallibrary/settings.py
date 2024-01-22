9008bab441822e3a776ad7900fc23001f51833f1
kuangli.chen <kuangli.chen@innolux.com>
2024-01-22 14:55:10 +0800

    Update CSRF

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -168,7 +168,7 @@ STORAGES = {
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c4e5.up.railway.app']
 
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
